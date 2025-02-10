d3d7706876442d012879ad06760b1143878b8fc4
Jason Ojisan <jason@fedora.email>
2025-02-10 09:13:55 -0500

    Rename rikaitan pdf viewer pdf
    * Update rikaitan pdf viewer update script
    
    * Update rikaitan pdf viewer
    
    * Fix permissions on run script
    
    <rikaitan.link>ZmJkZTdkYjM2ZjUzMjQ3ZmY4MmM1OTNlOTZkMzQxMTdkYzg1MzBiNQo=</rikaitan.link>

--- docs/rikaitan-pdf-viewer/update.sh
+++ docs/rikaitan-pdf-viewer/update.sh
@@ -5,6 +5,9 @@ wget "https://github.com/mozilla/pdf.js/releases/download/v$pdfjs_version/pdfjs-
 unzip -o "pdfjs-$pdfjs_version-dist.zip" "web/*" -d "."
 unzip -o "pdfjs-$pdfjs_version-dist.zip" "build/*" -d "."
 mv "./web/viewer.html" "./web/index.html"
-cp "rikaitan-pdf-viewer.pdf" "./web/compressed.tracemonkey-pldi-09.pdf"
+
+cp "rikaitan-pdf-viewer.pdf" "./web/rikaitan-pdf-viewer.pdf"
+sed -i -e 's/"compressed.tracemonkey-pldi-09.pdf"/"rikaitan-pdf-viewer.pdf"/g' ./web/viewer.mjs
+rm ./web/compressed.tracemonkey-pldi-09.pdf
 
 rm "pdfjs-$pdfjs_version-dist.zip"
